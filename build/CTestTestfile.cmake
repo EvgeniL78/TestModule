3eac281c86c85c26691a0eedfb5d62fe66e675d1
EvgeniL78 <lukin-e-piter@yandex.ru>
2023-12-15 15:36:20 +0300

    first

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/evgeni/Work2/TestModule
+# Build directory: /home/evgeni/Work2/TestModule/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
