09a75f1d7d316c34c2653b44ebaba161664071ea
EvgeniL78 <147860878+EvgeniL78@users.noreply.github.com>
2023-12-16 21:26:18 +0300

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/evgeni/Work2/TestModule
-# Build directory: /home/evgeni/Work2/TestModule/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
