fd3985d27d74a4a6b60f0b0d9ccf64472c8ef611
EvgeniL78 <lukin-e-piter@yandex.ru>
2023-12-17 20:21:04 +0300

    addition

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <OsWrapper.h>
 #endif
 #include <iostream>
+#include <type_traits>
 #include <string>
 #include <thread>
 #include <chrono>
@@ -25,14 +26,7 @@ map<int, shared_ptr<Automat>> autoStates = {
   { AUTOMAT_STATE_2, make_shared<AutomatState2>() },
   { AUTOMAT_STATE_OTHER, make_shared<AutomatStateOther>() }
 };
-shared_ptr<Automat> currAuto = autoStates[AUTOMAT_STATE_BEGIN];
-
-bool endOfWork = false;
-
-void printCurrAutomatState()
-{
-	cout << currAuto->GetStateMsg() << endl;
-}
+shared_ptr<Automat> currAuto;
 
 void setAutomatToState(const char* topic, int to_state)
 {
@@ -42,82 +36,80 @@ void setAutomatToState(const char* topic, int to_state)
   {
     switch(to_state)
     {
+      case AUTOMAT_STATE_BEGIN:
       case AUTOMAT_STATE_END:
       case AUTOMAT_STATE_2:
-      break;
+        break;
 
       case AUTOMAT_STATE_OTHER:
       default:
         to_state = AUTOMAT_STATE_OTHER;
-      break;
+        break;
     }
    
     currAuto = autoStates[to_state];
-    printCurrAutomatState();
-
-    if (to_state == AUTOMAT_STATE_END)
-      endOfWork = true;
+   	cout << currAuto->GetStateMsg() << endl;
   }
 }
 
 int main(int argc, char* argv[])
 {
-	printCurrAutomatState();
+  setAutomatToState(TOPIC_GO_TO_STATE, AUTOMAT_STATE_BEGIN);
 
   // Arg parser
 
-  enum { main_thread, stop_automat, just_exit };
+    string app;
+    switch (cmdArgsParser(argc, argv, app))
+    {
+      case EArgParams::log_none:
+        setPrintMode(EPrintMode::log_none);
+        break;
+      case EArgParams::log_base:
+        setPrintMode(EPrintMode::log_base);
+        break;
+      case EArgParams::log_all:
+        setPrintMode(EPrintMode::log_all);
+        break;
 
-  string app;
-  switch (cmdArgsParser(argc, argv, app))
-  {
-    case EArgParams::log_none:
-      break;
-      
-    case EArgParams::run_app:
-      runApp(app);
-    default:
-      throw stop_automat;
-      break;
-  }
+      case EArgParams::run_app:
+        runApp(app);
+      default:
+        setAutomatToState(TOPIC_GO_TO_STATE, AUTOMAT_STATE_END);
+        return 0;
+    }
 
-  // Mqtt client
+    // Mqtt client
 
-	Client client({TOPIC_STATE, TOPIC_GO_TO_STATE}, setAutomatToState);
+    Client client({TOPIC_STATE, TOPIC_GO_TO_STATE}, setAutomatToState);
 
-  // Main thread
+    // Main thread
 
-	printf("\nPress Q<Enter> to quit\n\n");
-
-  chrono::milliseconds timespan(500);
-  this_thread::sleep_for(timespan);
-  auto begin = std::chrono::high_resolution_clock::now();
-
-	do
-	{
-    // publish time from start
-
-    int64_t fromStart = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - begin).count();
-    string msg = to_string(fromStart);
-    client.Publish(TOPIC_TIME_ELAPSED, msg);
-
-    // publish current time
-
-    msg = getCurrTime();
-    client.Publish(TOPIC_TIME, msg);
-
+    chrono::milliseconds timespan(500);
     this_thread::sleep_for(timespan);
-	}
-  while (
-    !kbHitQ() || // Q
-    !endOfWork || // to end state
-    !client.IsFinished()); // client was broke
+    auto begin = std::chrono::high_resolution_clock::now();
 
-  if (!endOfWork) // if Q was pressed or client was broke
-  {
-    setAutomatToState(TOPIC_GO_TO_STATE, 1);
-	  printCurrAutomatState();
-  }
+    printLog(ELogType::plain, "\nPress Q<Enter> to quit\n\n");
+    do
+    {
+      // publish time from start
+
+      int64_t fromStart = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - begin).count();
+      string msg = to_string(fromStart);
+      client.Publish(TOPIC_TIME_ELAPSED, msg);
+
+      // publish current time
+
+      msg = getCurrTime();
+      client.Publish(TOPIC_TIME, msg);
+
+      //
+
+      if (kbHitQ())
+        setAutomatToState(TOPIC_GO_TO_STATE, AUTOMAT_STATE_END);
+
+      this_thread::sleep_for(timespan);
+    }
+    while (is_same_v<decltype(currAuto), AutomatEnd>);
 
  	return 0;
 }
